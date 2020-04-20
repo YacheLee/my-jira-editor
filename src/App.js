787647086795451899ebcc2789406de50088ce81
ya-che.li <ya-che.li@and.digital>
2020-04-20 12:09:40 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Editor} from '@atlaskit/editor-core';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <Editor appearance="comment"
+        onChange={(e)=>{
+            console.log(JSON.stringify(e.state.toJSON()));
+            debugger;
+        }}
+    />;
 }
 
 export default App;
